23bf8bd5c63148411feb70c1a714c3649b8e7580
gizmore <gizmore@wechall.net>
2021-07-08 08:47:44 +0200

    6.10.4-r1174

--- Module_CKEditor.php
+++ Module_CKEditor.php
@@ -3,20 +3,33 @@ namespace GDO\CKEditor;
 
 use GDO\Core\GDO_Module;
 use GDO\Language\GDO_Language;
+use GDO\UI\GDT_Message;
 
+/**
+ * CKEditor bindings.
+ * 
+ * @author gizmore
+ * @version 6.10.4
+ * @since 6.8.0
+ */
 final class Module_CKEditor extends GDO_Module
 {
     public function getDependencies() { return ['JQuery']; }
-    
     public function thirdPartyFolders() { return ['/ckeditor5/']; }
-    
     public function onLoadLanguage() { return $this->loadLanguage('lang/ckeditor'); }
-    
     public function getClasses()
     {
         return [
             GDO_CKFile::class,
         ];
+    }
+    
+    ############
+    ### Init ###
+    ############
+    public function onInit()
+    {
+        GDT_Message::$EDITOR_NAME = 'CKEditor';
     }
     
     public function onIncludeScripts()
